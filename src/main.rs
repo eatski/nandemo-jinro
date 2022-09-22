31079ad33e1c1917909cbeb3853f15ef455603a2
hagaitski <i.haga629@gmail.com>
2022-09-22 10:46:26 +0900

    fcに変更

--- src/main.rs
+++ src/main.rs
@@ -11,31 +11,16 @@ mod landing;
 mod router;
 mod storage;
 
-struct Root();
-
-impl Component for Root {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Root {
-        }
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, _: Self::Message) -> bool {
-        true
-    }
-
-    fn view(&self, _: &Context<Self>) -> Html {
-        html! {
-            <div>
-                {header()}
-                <BrowserRouter>
-                    <Switch<Route> render={Switch::render(switch)} />
-                </BrowserRouter>
-                {footer()}
-            </div>
-        }
+#[function_component(Root)]
+fn root() -> Html {
+    html! {
+        <div>
+            {header()}
+            <BrowserRouter>
+                <Switch<Route> render={Switch::render(switch)} />
+            </BrowserRouter>
+            {footer()}
+        </div>
     }
 }
 
