29df2bd2e02ceb8baa35d4ff1f0720d6099429fb
hagaitski <i.haga629@gmail.com>
2022-10-11 22:03:41 +0900

    aria-labelを追加

--- packages/rule_make/src/lib.rs
+++ packages/rule_make/src/lib.rs
@@ -103,7 +103,8 @@ pub fn rule_make(props: &Props) -> Html {
                                                 });
                                                 state.set(captured_state)
                                             })} 
-                                            class="text-black hover:text-black-light " 
+                                            class="text-black hover:text-black-light"
+                                            aria-label="役職を追加" 
                                         >
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
