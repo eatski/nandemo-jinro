5e32175204cb8e8bff5214058ca4fdb5955f946e
hagaitski <i.haga629@gmail.com>
2022-11-27 16:55:11 +0900

    app_rootを分離

---
+++ packages/app_root/src/lib.rs
@@ -0,0 +1,30 @@
+use components::layout::Layout;
+use components::room::Room;
+use landing::landing::Landing;
+use router::Route;
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+mod components;
+
+#[function_component]
+pub fn Root() -> Html {
+    html! {
+        <Layout>
+            <BrowserRouter>
+                <Switch<Route> render={switch} />
+            </BrowserRouter>
+        </Layout>
+    }
+}
+
+fn switch(route: Route) -> Html {
+    match route {
+        Route::Home => html! { <Landing /> },
+        Route::Room { id } => html! {
+            <Room room_id={id.to_string()}/>
+        },
+        Route::NotFound => html! { <p>{"ページが見つかりませんでした"}</p> },
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,4 @@
-use components::layout::Layout;
-use components::room::Room;
-use landing::landing::Landing;
-use router::Route;
-use yew::prelude::*;
-use yew_router::prelude::*;
-
-mod components;
-
-#[function_component(Root)]
-fn root() -> Html {
-    html! {
-        <Layout>
-            <BrowserRouter>
-                <Switch<Route> render={switch} />
-            </BrowserRouter>
-        </Layout>
-    }
-}
-
-fn switch(route: Route) -> Html {
-    match route {
-        Route::Home => html! { <Landing /> },
-        Route::Room { id } => html! {
-            <Room room_id={id.to_string()}/>
-        },
-        Route::NotFound => html! { <p>{"ページが見つかりませんでした"}</p> },
-    }
-}
+use app_root::Root;
 
 fn main() {
     yew::Renderer::<Root>::new().render();
