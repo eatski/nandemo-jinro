300fc7d57c91fa69350ce4a9ddce78379dcd03dc
hagaitski <i.haga629@gmail.com>
2022-12-08 00:59:54 +0900

    テキストの変更

--- packages/app_root/src/components/layout.rs
+++ packages/app_root/src/components/layout.rs
@@ -8,17 +8,18 @@ pub struct ChildrenOnlyProps {
 
 #[function_component(Layout)]
 pub fn layout(props: &ChildrenOnlyProps) -> Html {
-    let dark_mode = use_state(|| false);
+    let dark_mode_state = use_state(|| false);
+    let dark_mode = *dark_mode_state;
     html! {
-        <div class="bg-screen min-h-screen h-max" data-theme={if *dark_mode {"dark"} else { "light"}}>
+        <div class="bg-screen min-h-screen h-max" data-theme={if dark_mode {"dark"} else { "light"}}>
             <header class="w-full border-separator border-solid border-b px-4 py-3 ">
                 <div class="flex items-center">
                     <a class="font-mono text-word text-lg" href="/">
                         {"なんでも人狼"}
                     </a>
                     <div class="ml-auto">
-                        <ButtonSub onclick={Callback::from(move |_| dark_mode.set(!*dark_mode))}>
-                            {"ダークモード"}
+                        <ButtonSub onclick={Callback::from(move |_| dark_mode_state.set(!*dark_mode_state))}>
+                            {if dark_mode {"ライトモード"} else { "ダークモード"}}
                         </ButtonSub>
                         {button_link("報告","https://github.com/eatski/roleroll/issues/new")}
                     </div>
