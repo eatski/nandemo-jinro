d573bfa77fa1742cab318821c08186beae546953
hagaitski <i.haga629@gmail.com>
2022-12-17 15:42:25 +0900

    部屋が見つからない場合にエラーにする

--- packages/landing/src/entrance.rs
+++ packages/landing/src/entrance.rs
@@ -35,7 +35,7 @@ pub fn guest_entrance(props: &GuestEntranceProps) -> Html {
     });
 
     let host = use_collection::<MemberJSON>(&props.room_id)
-        .map(|memebers| memebers.into_iter().find(|m| m.is_host).unwrap());
+        .map(|memebers| memebers.into_iter().find(|m| m.is_host));
 
     let room_state = use_historical_read::<RoomEditAction>(props.room_id.clone());
 
@@ -44,29 +44,41 @@ pub fn guest_entrance(props: &GuestEntranceProps) -> Html {
     match state {
         Result::Err(NotFetched::Loading) => loading(),
         Result::Ok((room, host)) => {
-            if room.latest.can_join {
-                html! {
-                    <>
-                        {title()}
-                        <section class="mx-auto w-full max-w-2xl">
-                            <Heading2>{ format!("「{}」の部屋に入る",host.name)}</Heading2>
-                            <div class="mt-3">
-                                <JoinForm label="参加" default="" form_label="名前を入力して参加する" placeholder="あなたの名前" onsubmit={add_member} />
-                            </div>
-
-                        </section>
-                    </>
+            if let Some(host) = host {
+                if room.latest.can_join {
+                    html! {
+                        <>
+                            {title()}
+                            <section class="mx-auto w-full max-w-2xl">
+                                <Heading2>{ format!("「{}」の部屋に入る",host.name)}</Heading2>
+                                <div class="mt-3">
+                                    <JoinForm label="参加" default="" form_label="名前を入力して参加する" placeholder="あなたの名前" onsubmit={add_member} />
+                                </div>
+    
+                            </section>
+                        </>
+                    }
+                } else {
+                    html! {
+                        <div>
+                            {title()}
+                            <p class="mx-auto w-full max-w-2xl text-center text-word">
+                                {"この部屋は参加を締め切られました。"}
+                            </p>
+                        </div>
+                    }
                 }
             } else {
                 html! {
                     <div>
                         {title()}
                         <p class="mx-auto w-full max-w-2xl text-center text-word">
-                            {"この部屋は参加を締め切られました。"}
+                            {"部屋が見つかりませんでした。"}
                         </p>
                     </div>
                 }
             }
+            
         },
         Result::Err(NotFetched::Error) => {
             unexpected_error()
